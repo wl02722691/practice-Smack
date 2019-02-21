270aced1c2448d2f9086f717f641929d29fb1308
張書涵 <zhangshuhan@zhangshuhans-MacBook-Pro.local>
2019-02-21 12:08:27 +0800

    finish burger list

---
+++ smack/Controller/ChannelVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChannelVC.swift
+//  smack
+//
+//  Created by 張書涵 on 2019/2/21.
+//  Copyright © 2019 張書涵. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ smack/Controller/ChatVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by 張書涵 on 2019/2/21.
+//  Copyright © 2019 張書涵. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+
+
+}
