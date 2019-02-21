a330dc186bd6cc393145ae0ba0a05566ee32ed8d
張書涵 <zhangshuhan@zhangshuhans-MacBook-Pro.local>
2019-02-21 12:20:44 +0800

    Change rearViewRevealWidth

--- smack/Controller/ChannelVC.swift
+++ smack/Controller/ChannelVC.swift
@@ -13,7 +13,7 @@ class ChannelVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.width - 60
     }
     
 
