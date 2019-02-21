2cf25341eecdd03c92bd20e81de9864887d25fba
張書涵 <zhangshuhan@zhangshuhans-MacBook-Pro.local>
2019-02-21 12:56:10 +0800

    Add GradientView

---
+++ smack/View/GradientView.swift
@@ -0,0 +1,37 @@
+//
+//  GradientView.swift
+//  smack
+//
+//  Created by 張書涵 on 2019/2/21.
+//  Copyright © 2019 張書涵. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 1, green: 0.4588235294, blue: 0.5490196078, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 1, green: 0.4941176471, blue: 0.7019607843, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor,bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0 , y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+    
+
+}
